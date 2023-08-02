d9d98cca9526a2d0b9435a415a51102716a028cc
Monique <skaiwilliams85@gmail.com>
2023-08-02 10:51:16 -0500

    built stacks & queue with linked list

---
+++ queue/queueWithLinkedList.js
@@ -0,0 +1,64 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.first;
+  }
+
+  enqueue(value) {
+    const node = new Node(value);
+
+    if (this.length === 0) {
+      this.first = node;
+      this.last = node;
+    }
+
+    this.last.next = node;
+    this.last = node;
+    this.length++;
+    return this;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+    if (this.top === this.bottom) {
+      this.last = null;
+    }
+
+    const second = this.first.next;
+    this.first.next = null;
+    this.first = second;
+
+    this.length--;
+    return this;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+}
+
+const queue = new Queue();
+queue.enqueue("joy");
+queue.enqueue("matt");
+queue.enqueue("pavel");
+queue.enqueue("samir");
+console.log(queue);
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+
+console.log(queue);
+// joy, matt, pavel, samir
---
+++ stack/stackWithLinkedList.js
@@ -0,0 +1,59 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.top;
+  }
+
+  // the list is backwards
+  push(value) {
+    const node = new Node(value);
+
+    if (this.length === 0) {
+      this.top = node;
+      this.bottom = node;
+    } else {
+      const holdingPointer = this.top;
+      this.top = node;
+      this.top.next = holdingPointer;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.top) return null;
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+
+    this.top = this.top.next;
+    this.length--;
+    return this;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+}
+
+const stack = new Stack();
+
+stack.push("google");
+stack.push("udemy");
+stack.push("discord");
+stack.push("Pinterest");
+stack.pop();
+console.log(stack);
